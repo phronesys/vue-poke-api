832b45cae8926e53bf0a4604c305438ced164e93
phronesys <daniel.dazarola@gmail.com>
2021-02-06 09:17:51 -0300

    first commit, base

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">the header</div>
+  <div class="main">
+    <ul class="container">
+      <li class="item" v-for="pokemon in pokemones" :key="pokemon">
+        {{ pokemon.url }}
+      </li>
+    </ul>
+  </div>
+  <div class="footer">the footer</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      pokemones: [],
+    };
+  },
+  created() {
+    const axios = require("axios").default;
+    const pokemonsitos = async () => {
+      try {
+        const res = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon?limit=151"
+        );
+        console.log(res.data.results);
+        this.pokemones = res.data.results;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    pokemonsitos();
+  },
+};
 </script>
 
 <style>
@@ -22,5 +43,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  margin: -0.5rem;
+}
+.item {
+  width: 100px;
+  list-style: none;
+  border: 1px solid black;
+  margin: 0.5rem;
+  padding: 5rem;
 }
 </style>
